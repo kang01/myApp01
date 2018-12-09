6883be05c6fb940ca1258914d2d95265ef094acb
kang01 <904451260@qq.com>
2018-12-09 16:02:59 +0800

    实例修改

--- src/app/contact/contact.page.ts
+++ src/app/contact/contact.page.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 declare const Konva: any;
+declare const gifler: any;
 @Component({
   selector: 'app-contact',
   templateUrl: 'contact.page.html',
@@ -10,129 +11,37 @@ export class ContactPage {
   ngOnInit(){
     var width = window.innerWidth;
     var height = window.innerHeight;
-    function addNode(obj, layer) {
-      var node = new Konva.Circle({
-          x: obj.x,
-          y: obj.y,
-          radius: 4,
-          fill: obj.color,
-          id: obj.id
-      });
 
-      layer.add(node);
+    var stage = new Konva.Stage({
+        container: 'container1',
+        width: width,
+        height: height
+    });
+
+    var layer = new Konva.Layer();
+    stage.add(layer);
+
+
+    var canvas = document.createElement('canvas');
+    // use external library to parse and draw gif animation
+    function onDrawFrame(ctx, frame) {
+        // update canvas size
+        canvas.width = frame.width;
+        canvas.height = frame.height;
+        // update canvas that we are using for Konva.Image
+        ctx.drawImage(frame.buffer, 0, 0);
+        // redraw the layer
+        layer.draw();
+    }
+
+    new gifler('/assets/images/1123.gif')
+        .frames(canvas, onDrawFrame);
+
+    // draw resulted canvas into the stage as Konva.Image
+    var image = new Konva.Image({
+        image: canvas
+    });
+    layer.add(image);
+
   }
-  var stage = new Konva.Stage({
-      container: 'container',
-      width: width,
-      height: height,
-      draggable: true
-  });
-
-  var tooltipLayer = new Konva.Layer();
-  var dragLayer = new Konva.Layer();
-
-  var tooltip = new Konva.Label({
-      opacity: 0.75,
-      visible: false,
-      listening: false
-  });
-
-  tooltip.add(new Konva.Tag({
-      fill: 'black',
-      pointerDirection: 'down',
-      pointerWidth: 10,
-      pointerHeight: 10,
-      lineJoin: 'round',
-      shadowColor: 'black',
-      shadowBlur: 10,
-      shadowOffset: 10,
-      shadowOpacity: 0.2
-  }));
-
-  tooltip.add(new Konva.Text({
-      text: '',
-      fontFamily: 'Calibri',
-      fontSize: 18,
-      padding: 5,
-      fill: 'white'
-  }));
-
-  tooltipLayer.add(tooltip);
-
-  // build data
-  var data = [];
-
-  var colors = ['red', 'orange', 'cyan', 'green', 'blue', 'purple'];
-  for(var n = 0; n < 20000; n++) {
-      var x = Math.random() * width;
-      var y = height + (Math.random() * 200) - 100 + (height / width) * -1 * x;
-      data.push({
-          x: x,
-          y: y,
-          id: n,
-          color: colors[Math.round(Math.random() * 5)]
-      });
-  }
-
-  // render data
-  var nodeCount = 0;
-  var layer = new Konva.Layer();
-  for(var n = 0; n < data.length; n++) {
-      addNode(data[n], layer);
-      nodeCount++;
-      if(nodeCount >= 1000) {
-          nodeCount = 0;
-          stage.add(layer);
-          layer = new Konva.Layer();
-      }
-  }
-
-  stage.add(dragLayer);
-  stage.add(tooltipLayer);
-
-  stage.on('mouseover mousemove dragmove', function(evt) {
-      var node = evt.target;
-      if (node) {
-          // update tooltip
-          var mousePos = node.getStage().getPointerPosition();
-          tooltip.position({
-              x : mousePos.x,
-              y : mousePos.y - 5
-          });
-          tooltip.getText().setText("node: " + node.getId() + ", color: " + node.getFill());
-          tooltip.show();
-          tooltipLayer.batchDraw();
-      }
-  });
-
-  stage.on('mouseout', function(evt) {
-      tooltip.hide();
-      tooltipLayer.draw();
-  });
-
-  var startLayer;
-
-  stage.on('mousedown', function(evt) {
-      var shape = evt.target;
-      if (shape) {
-          startLayer = shape.getLayer();
-          shape.moveTo(dragLayer);
-          startLayer.draw();
-          // manually trigger drag and drop
-          shape.startDrag();
-      }
-  });
-
-  stage.on('mouseup', function(evt) {
-      var shape = evt.target;
-      if (shape) {
-          shape.moveTo(startLayer);
-          dragLayer.draw();
-          startLayer.draw();
-      }
-  });
-  }
-
-  
-
 }
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -3,6 +3,7 @@ import { of, Observable, BehaviorSubject } from 'rxjs';
 import { KonvaComponent } from 'ng2-konva';
 
 declare const Konva: any;
+declare const gifler: any;
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
@@ -10,11 +11,16 @@ declare const Konva: any;
 })
 export class HomePage {
     stage;
+    blobs = [];
     f1Data = [
-        {name:'冰箱',x:10,y:347,width:40,height:63,url:'/assets/images/冰箱2.5D.png'},
+        {name:'冰箱',x:10,y:347,width:40,height:63,url:'/assets/images/1122.gif'},
         {name:'冰箱',x:-20,y:364,width:40,height:63,url:'/assets/images/冰箱2.5D.png'},
+
+        {name:'冰箱',x:217,y:535,width:40,height:63,url:'/assets/images/冰箱2.5D.png'},
+        {name:'冰箱',x:247,y:552,width:40,height:63,url:'/assets/images/冰箱2.5D.png'},
+
         // L1-top
-        {name:'冰箱',x:127,y:279,width:40,height:63,url:'/assets/images/冰箱2.5D.png'},
+        {name:'冰箱',x:127,y:279,width:40,height:63,url:'/assets/images/1122.gif'},
         {name:'冰箱',x:157,y:296,width:40,height:63,url:'/assets/images/冰箱2.5D.png'},
         {name:'冰箱',x:186,y:313,width:40,height:63,url:'/assets/images/冰箱2.5D.png'},
         {name:'冰箱',x:216,y:331,width:40,height:63,url:'/assets/images/冰箱2.5D.png'},
@@ -27,7 +33,7 @@ export class HomePage {
         {name:'冰箱',x:186,y:347,width:40,height:63,url:'/assets/images/冰箱2.5D.png'},
         {name:'冰箱',x:216,y:364,width:40,height:63,url:'/assets/images/冰箱2.5D.png'},
         // R1-top
-        {name:'冰箱',x:493,y:383,width:40,height:63,url:'/assets/images/冰箱2.5D.png'},
+        {name:'冰箱',x:393,y:435,width:40,height:63,url:'/assets/images/冰箱2.5D.png'},
         {name:'冰箱',x:423,y:450,width:40,height:63,url:'/assets/images/冰箱2.5D.png'},
         {name:'冰箱',x:453,y:467,width:40,height:63,url:'/assets/images/冰箱2.5D.png'},
         {name:'冰箱',x:482,y:484,width:40,height:63,url:'/assets/images/冰箱2.5D.png'},
@@ -41,7 +47,52 @@ export class HomePage {
         {name:'冰箱',x:452,y:501,width:40,height:63,url:'/assets/images/冰箱2.5D.png'},
         {name:'冰箱',x:482,y:518,width:40,height:63,url:'/assets/images/冰箱2.5D.png'},
         {name:'冰箱',x:512,y:535,width:40,height:63,url:'/assets/images/冰箱2.5D.png'},
-    ]
+
+        // L1-冻存罐 top
+        {name:'冻存罐',x:269,y:206,width:51,height:63,url:'/assets/images/冰箱2.5D.png'},
+        {name:'冻存罐',x:313,y:231,width:51,height:63,url:'/assets/images/冰箱2.5D.png'},
+        {name:'冻存罐',x:358,y:256,width:51,height:63,url:'/assets/images/冰箱2.5D.png'},
+        {name:'冻存罐',x:401,y:282,width:51,height:63,url:'/assets/images/冰箱2.5D.png'},
+        // L1-冻存罐 bottom
+        {name:'冻存罐',x:225,y:230,width:51,height:63,url:'/assets/images/冰箱2.5D.png'},
+        {name:'冻存罐',x:270,y:255,width:51,height:63,url:'/assets/images/冰箱2.5D.png'},
+        {name:'冻存罐',x:315,y:281,width:51,height:63,url:'/assets/images/冰箱2.5D.png'},
+        {name:'冻存罐',x:359,y:306,width:51,height:63,url:'/assets/images/冰箱2.5D.png'},
+
+        // // R1-冻存罐 top
+        {name:'冻存罐',x:534,y:360,width:51,height:63,url:'/assets/images/冰箱2.5D.png'},
+        {name:'冻存罐',x:578,y:385,width:51,height:63,url:'/assets/images/冰箱2.5D.png'},
+        {name:'冻存罐',x:623,y:410,width:51,height:63,url:'/assets/images/冰箱2.5D.png'},
+        {name:'冻存罐',x:661,y:436,width:51,height:63,url:'/assets/images/冰箱2.5D.png'},
+        // R1-冻存罐 bottom
+        {name:'冻存罐',x:490,y:384,width:51,height:63,url:'/assets/images/冰箱2.5D.png'},
+        {name:'冻存罐',x:535,y:409,width:51,height:63,url:'/assets/images/冰箱2.5D.png'},
+        {name:'冻存罐',x:580,y:435,width:51,height:63,url:'/assets/images/冰箱2.5D.png'},
+        {name:'冻存罐',x:624,y:460,width:51,height:63,url:'/assets/images/冰箱2.5D.png'},
+       
+        // Lb-冻存罐 top
+        {name:'冻存罐',x:357,y:153,width:51,height:63,url:'/assets/images/冰箱2.5D.png'},
+        {name:'冻存罐',x:402,y:179,width:51,height:63,url:'/assets/images/冰箱2.5D.png'},
+        {name:'冻存罐',x:446,y:204,width:51,height:63,url:'/assets/images/冰箱2.5D.png'},
+        // Rb-冻存罐 bottom
+        {name:'冻存罐',x:578,y:281,width:51,height:63,url:'/assets/images/冰箱2.5D.png'},
+        {name:'冻存罐',x:623,y:307,width:51,height:63,url:'/assets/images/冰箱2.5D.png'},
+        {name:'冻存罐',x:667,y:332,width:51,height:63,url:'/assets/images/冰箱2.5D.png'},
+        {name:'冻存罐',x:712,y:357,width:51,height:63,url:'/assets/images/冰箱2.5D.png'},
+        {name:'冻存罐',x:756,y:383,width:51,height:63,url:'/assets/images/冰箱2.5D.png'},
+    ];
+    canvas = document.createElement('canvas');
+    // canvas.width = 50;
+    onDrawFrame(ctx,frame,group) {
+        // var ctx=this.canvas.getContext('2d');
+        // update canvas size
+        // this.canvas.width = 50;
+        // this.canvas.height = 60;
+        // update canvas that we are using for Konva.Image
+        ctx.drawImage(frame.buffer, 0, 0);
+        // redraw the layer
+        group.draw();
+    }
     addFreezerImage(group,data){
         // var randX = Math.random() * this.stage.getWidth();
         // var randY = Math.random() * this.stage.getHeight();    
@@ -50,15 +101,27 @@ export class HomePage {
             y: data.y,
             width: data.width,
             height: data.height,
-            draggable: true
         }); 
-        var imageObj2 = new Image();
-        imageObj2.onload = function() {
-            freezerImg.image(imageObj2);
-            // layer1.draw();
-        };
-        imageObj2.src = data.url;
-        group.add(freezerImg);
+        if(data.flag === '1'){
+            new gifler('/assets/images/1122.gif')
+            .frames(this.canvas,this.onDrawFrame);
+            var image2 = new Konva.Image({
+                image: this.canvas
+            });
+            // freezerImg.image(image2);
+            // group.add(freezerImg);
+            group.add(image2);
+        }else{
+            var imageObj2 = new Image();
+            imageObj2.onload = function() {
+                freezerImg.image(imageObj2);
+            };
+            imageObj2.src = data.url;
+            group.add(freezerImg);
+            this.blobs.push(freezerImg);
+        }
+       
+        
     }
     ngOnInit(){
         var width = window.innerWidth;
@@ -86,7 +149,7 @@ export class HomePage {
         };
         imageObj1.src = '/assets/images/biobank-f1.svg';
         var group = new Konva.Group({
-            draggable: true
+           
         });
             for(var n = 0; n < this.f1Data.length; n++) {
                 // this.addFreezerImage(layer);
@@ -95,11 +158,37 @@ export class HomePage {
         layer.add(group);
         this.stage.add(layer);
 
+        group.on('mouseover', function(evt) {
+            var shape = evt.target;
+            console.log(shape);
+            document.body.style.cursor = 'pointer';
+            shape.scaleX(1.2);
+            shape.scaleY(1.2);
+            layer.draw();
+        });
+        group.on('mouseout', function(evt) {
+            var shape = evt.target;
+            document.body.style.cursor = 'default';
+            shape.scaleX(1);
+            shape.scaleY(1);
+            layer.draw();
+        });
+        var period = 2000;
+        var centerTension = 0;
+        var amplitude = 1;
+
+        var anim = new Konva.Animation(function(frame) {
+            for(var n = 0; n < this.blobs.length; n++) {
+                this.blobs[n].scaleX(1.2);;
+                this.blobs[n].scaleY(1.2);;
+            }
+        }, layer);
+
+        anim.start();
 
 
         var scaleBy = 1.1;
         window.addEventListener('wheel', (e) => {
-            console.log("aaaa")
             e.preventDefault();
             var oldScale = this.stage.scaleX();
             var mousePointTo = {
